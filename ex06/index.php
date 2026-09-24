4221671cc89508c1022ec51f458248b78c06cdbf
Alvaro Contreras <alcoga2@jviladoms.cat>
2026-09-24 10:50:30 +0200

    24/09/2026 - 10:50 - Act EX06

--- ex06/index.php
+++ ex06/index.php
@@ -1,7 +1,11 @@
 <?php
 $preu = 99.99;
+$preu_decimal = number_format($preu, 2, ',', '.');
 const IVA = 0.21;
+$valor_IVA = $preu * IVA;
+$valor_IVA_decimal = number_format($valor_IVA, 2 , ',' ,'.');
 $total = $preu * (1+(IVA));
+$total_decimal = number_format($total, 2, ',', '.');
 $producte = 'Camiseta chupi chupi-guay';
 
 const BOTIGA = 'Tienda online guay';
@@ -28,14 +32,16 @@ const DESCOMPTE_SOCI = null;
     <article class="producte">
       <h2>Cami Guay</h2>
       <p class="descripció"><?= $producte ?></p>
-      <p class="preu">El preu sense IVA: <?= $preu . ' ' . MONEDA?></p>
-      <p class="preu">IVA (21%): <?= round($preu * IVA, 3) ?></p>
-      <p class="total">TOTAL: <?= $total . ' ' . MONEDA ?> </p>
+      <p class="preu">El preu sense IVA: <?= $preu_decimal . ' ' . MONEDA?></p>
+      <p class="preu">IVA (21%): <?= $valor_IVA_decimal . ' ' . MONEDA?></p>
+      <p class="total">TOTAL: <?= $total_decimal . ' ' . MONEDA ?> </p>
       <p class="estoc">Unitats disponible: 5</p>
       <p class="ref">CAM-1425367</p>
     </article>
   </main>
 
+  <?php # echo MONEDA = 'HAPAAA'?>
+  <!-- Parse error: syntax error, unexpected token "=", expecting "," or ";" in C:\xampp\htdocs\M0613\classe\exercicis_php\ex06\index.php on line 38 -->
   <footer>
     <p>Footer de la tienda chupi guay S.L</p>
   </footer>
